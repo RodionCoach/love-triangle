c076db84de0f24dc43e7bc9a20d422e411c96540
Rodion Kochetkov <www.radik@yandex.by>
2019-02-21 00:43:52 +0300

    task *love-triangles* is completed!

--- src/index.js
+++ src/index.js
@@ -4,4 +4,16 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  let length = preferences.length, count = 0, a, b, c;
+  if(preferences.length < 2) return 0;
+  for(let i = 0; i <= length; i++){
+    b = preferences[i];
+    c = preferences[b - 1];
+    a = preferences[c - 1];
+    if(b > a &&  a < c && a - 1 == i){
+      count++;
+    }
+  }
+  return count;
 };
+//console.log(getLoveTrianglesCount([6, 14, 12, 21, 3, 20, 7, 15, 16, 16, 14, 4, 11, 13, 18, 2, 1, 8, 3, 1, 12]))
